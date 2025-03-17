837d19347794728da681b17064f5da6210926c62
paulocesarvasco <paulocvasco.93@gmail.com>
2025-03-17 18:18:15 -0300

    Get pairs from environment variables

--- src/mb_mms/services/mb_api/commands.py
+++ src/mb_mms/services/mb_api/commands.py
@@ -1,3 +1,4 @@
+import os
 import click
 import time
 
@@ -17,24 +18,28 @@ def populate_db():
     end_time_unix = int(time.mktime(datetime.strptime(end_time, "%Y-%m-%d").timetuple()))
     start_time_unix = int(time.mktime(datetime.strptime(start_time, "%Y-%m-%d").timetuple()))
 
-    rates = mb.request_rate(pair='BRLBTC', start=start_time_unix, end=end_time_unix)
-    mms_20 = mb.calculate_mms(delta=20, rates=rates)
-    mms_50 = mb.calculate_mms(delta=50, rates=rates)
-    mms_200 = mb.calculate_mms(delta=200, rates=rates)
+    pairs = os.getenv('PAIRS', '').split(',')
 
-    if len(mms_20) != len(mms_50) != len(mms_200):
-        click.echo(message='inconsistent rates', err=True)
-        return
+    for pair in pairs:
+        rates = mb.request_rate(pair=pair, start=start_time_unix, end=end_time_unix)
+        mms_20 = mb.calculate_mms(delta=20, rates=rates)
+        mms_50 = mb.calculate_mms(delta=50, rates=rates)
+        mms_200 = mb.calculate_mms(delta=200, rates=rates)
 
-    with Session(db.get_db_engine()) as session, session.begin():
-        try:
-            for idx in range(len(mms_20)):
-                obj = MovingAverage()
-                obj.mms_20, obj.mms_50, obj.mms_200 = mms_20[idx][0], mms_50[idx][0], mms_200[idx][0]
-                obj.timestamp, obj.pair = mms_20[idx][1], 'BRLBTC'
-                session.add(obj)
-            session.commit()
-            click.echo('Database populated.')
-        except Exception as err:
-            session.rollback()
-            click.echo(message=err, err=True)
+        if len(mms_20) != len(mms_50) != len(mms_200):
+            click.echo(message='inconsistent rates', err=True)
+            return
+
+        with Session(db.get_db_engine()) as session, session.begin():
+            try:
+                for idx in range(len(mms_20)):
+                    obj = MovingAverage()
+                    obj.mms_20, obj.mms_50, obj.mms_200 = mms_20[idx][0], mms_50[idx][0], mms_200[idx][0]
+                    obj.timestamp, obj.pair = mms_20[idx][1], pair
+                    session.add(obj)
+                session.commit()
+            except Exception as err:
+                session.rollback()
+                click.echo(message=err, err=True)
+
+        click.echo('Database populated.')

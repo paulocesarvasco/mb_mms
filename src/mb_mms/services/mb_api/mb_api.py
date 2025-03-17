e2e37dc7b29b397599a3e38d080191e4f94db09e
paulocesarvasco <paulocvasco.93@gmail.com>
2025-03-17 02:36:54 -0300

    Implement method to calculate mms from registers

--- src/mb_mms/services/mb_api/mb_api.py
+++ src/mb_mms/services/mb_api/mb_api.py
@@ -1,4 +1,3 @@
-import datetime
 import os
 from requests import HTTPError, Session
 from http import HTTPStatus
@@ -9,10 +8,7 @@ class MB_API:
 
     def request_rate(self, pair:str, start, end):
         try:
-            unix_start = int(datetime.datetime.strptime(start, '%d-%m-%Y').timestamp())
-            unix_end = int(datetime.datetime.strptime(end, '%d-%m-%Y').timestamp())
-
-            url = os.getenv('MB_API', '').format(pair, unix_start, unix_end)
+            url = os.getenv('MB_API', '').format(pair, start, end)
             session = Session()
             session.headers.update({'User-Agent': 'Dummy User Agent'})
             res = session.get(url)
@@ -21,7 +17,8 @@ class MB_API:
                 print(res.text)
                 res.raise_for_status()
 
-            return res.json()
+            data = res.json()
+            return [(register['close'], register['timestamp']) for register in data['candles']]
 
         except ValueError as err:
             print('parameters error:', err)
@@ -29,3 +26,14 @@ class MB_API:
             print('request error:', err)
         except Exception as err:
             print('unexpected error:', err)
+
+    def calculate_mms(self, rates=None, delta=30):
+        assert rates is not None
+        mms = []
+        for idx in range(len(rates)):
+            if idx < delta:
+                mms.append((rates[idx][1], None))
+                continue
+            slice_rates = rates[idx-delta:idx]
+            mms.append((rates[idx][1], sum([register[0] for register in slice_rates])/delta))
+        return mms

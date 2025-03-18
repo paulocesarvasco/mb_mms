d18ab1db1ad4d2bf5d1a4f907669ebcc1b151ef1
paulocesarvasco <paulocvasco.93@gmail.com>
2025-03-18 14:59:25 -0300

    Fix default logic for end query parameter

--- mb_mms/api/routes.py
+++ mb_mms/api/routes.py
@@ -15,21 +15,25 @@ def root():
 def search(pair):
     mb = mb_api.MB_API()
 
-    default = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
-    default_unix = int(time.mktime(datetime.strptime(default, "%Y-%m-%d").timetuple()))
-
     try:
         precision = request.args.get('precision', type=str)
         start = request.args.get('from', type=int)
-        end = request.args.get('to', type=int, default=default_unix)
+        end = request.args.get('to', type=int)
 
-        if precision is None or start is None or end is None:
+        if precision is None or start is None:
             raise ValueError
+
+        if end is None:
+            default = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+            default_unix = int(time.mktime(datetime.strptime(default, "%Y-%m-%d").timetuple()))
+            end = default_unix
 
         precision = int(precision[:-1])
         res = mb.search_mms(pair=pair, start=start, end=end, precision=precision)
 
         return jsonify(res)
 
+    except ValueError:
+        return 'missed mandatory query parameters', HTTPStatus.BAD_REQUEST
     except Exception as err:
         return str(err), HTTPStatus.INTERNAL_SERVER_ERROR

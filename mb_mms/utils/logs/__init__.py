0e268d8c6e754ee37f35e6e22b3cab2f1cefb883
paulocesarvasco <paulocvasco.93@gmail.com>
2025-03-24 16:53:43 -0300

    Add specific logger for client requests

--- mb_mms/utils/logs/__init__.py
+++ mb_mms/utils/logs/__init__.py
@@ -1,12 +1,22 @@
 import os
 import sys
 from logging.config import dictConfig
+from logging import Formatter
+from flask import request
+
+class RequestFormatter(Formatter):
+    """Custom formatter that adds HTTP request details"""
+    def format(self, record):
+        record.url = request.url if request else 'no-request'
+        record.method = request.method if request else 'no-method'
+        record.remote_addr = request.remote_addr if request else 'no-ip'
+        return super().format(record)
 
 os.makedirs('logs', exist_ok=True)
 
 dictConfig({
     'version': 1,
-    'disable_existing_loggers': False,
+    'disable_existing_loggers': True,
     'formatters': {
         'default': {
             'format': '[%(asctime)s][%(levelname)s][%(module)s][%(message)s]',
@@ -15,7 +25,12 @@ dictConfig({
         'error': {
             'format': '[%(asctime)s][%(levelname)s][%(module)s][%(lineno)d][%(message)s]',
             'datefmt': '%Y-%m-%d %H:%M:%S'
-        }
+        },
+        'request_format': {
+                '()': RequestFormatter,
+                'format': '[%(asctime)s][%(levelname)s][%(module)s][%(method)s][%(url)s][%(remote_addr)s][%(message)s]',
+                'datefmt': '%Y-%m-%d %H:%M:%S'
+            },
     },
     'handlers': {
         'stdout': {
@@ -25,9 +40,11 @@ dictConfig({
             'level': 'INFO'
         },
         'file_debug': {
-            'class': 'logging.FileHandler',
+            'class': 'logging.handlers.RotatingFileHandler',
             'filename': 'logs/debug.log',
+            'maxBytes': 1024*1024,
             'formatter': 'default',
+            'backupCount': 3,
             'level': 'DEBUG'
         },
         'file_error': {
@@ -35,13 +52,26 @@ dictConfig({
             'filename': 'logs/error.log',
             'formatter': 'error',
             'level': 'ERROR'
+        },
+        'views_handler': {
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': 'logs/views.log',
+                'maxBytes': 1024*1024,
+                'backupCount': 3,
+                'formatter': 'request_format',
+                'level': 'DEBUG'
         }
     },
+    'root': {
+        'handlers': ['stdout', 'file_debug', 'file_error'],
+        'level': 'DEBUG',
+        'propagate': True
+    },
     'loggers': {
-        '': {
-            'handlers': ['stdout', 'file_debug', 'file_error'],
+        'views': {
+            'handlers': ['stdout', 'views_handler'],
             'level': 'DEBUG',
-            'propagate': True
+            'propagate': False
         }
     }
 })
